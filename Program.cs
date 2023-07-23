8d2f1053c67e050cf23c06a2a3da596e518d76a7
alejoc94 <alejoc94@gmail.com>
2023-07-22 20:54:19 -0500

    PRUEBA
    P

--- Program.cs
+++ Program.cs
@@ -1,16 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-//PUNTO 17
-using PRUEBA_SIESA;
+﻿using PRUEBA_SIESA;
 
-//PUNTO_17 p17 = new PUNTO_17();
+Console.WriteLine("PUNTO 17");
+PUNTO_17 p17 = new PUNTO_17();
 
+Console.WriteLine("PUNTO 18");
 PUNTO_18 p18 = new PUNTO_18();
 
-/*
-punto_5 p5 = new punto_5();
-var res5 = p5.punto5(5);
-Console.WriteLine("PUNTO 5");
-Console.WriteLine(Convert.ToString(res5));
-
-*/
-
+Console.WriteLine("PUNTO 19");
+PUNTO_19 p19 = new PUNTO_19();
